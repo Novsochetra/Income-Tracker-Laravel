fbaed481621acc214c0932d74d94d3c2dc3b5b48
Soben <ksoben.student@puthisastra.edu.kh>
2020-07-19 14:57:37 +0700

    Generate login and registration in Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
